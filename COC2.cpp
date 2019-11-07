4bbce944a350f100a113f24b686d7a738763bc32
Shutaro Hashimoto <shutaro_itpc_coconatu3@outlook.jp>
2019-11-07 21:22:32 +0900

    add file COC2.cpp

---
+++ COC2.cpp
@@ -0,0 +1,25 @@
+#include "COC2.h"
+
+int XXX::settotalscore(int SScore1){
+    return totalscore = SScore1;
+}
+
+int XXX::gettotalscore() {
+    return totalscore;
+}
+
+int YYY::settotalscore(int SScore2){
+    return totalscore = SScore2;
+}
+
+int YYY::gettotalscore() {
+    return totalscore;
+}
+
+int ZZZ::settotalscore(int SScore3){
+    return totalscore = SScore3;
+}
+
+int ZZZ::gettotalscore() {
+    return totalscore;
+}
